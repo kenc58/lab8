49d0a200b630aa46799b4fe7ed3473174735f162
Kenton Chick <kchick@ucsd.edu>
2019-02-28 11:30:17 -0800

    lab 8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+
+	L.mapquest.key = elqQOcyk7ldp8xvMCdxklyWmC8vEXtY3;
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.titleLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
